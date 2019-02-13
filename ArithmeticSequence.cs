b12e4fa1888de4f91fa334991bb988ed34435b62
egor-sudko <47115510+egor-sudko@users.noreply.github.com>
2019-02-13 21:19:59 +0300

    Update ArithmeticSequence.cs

--- ArithmeticSequence.cs
+++ ArithmeticSequence.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace PadawansTask2
 {
@@ -6,8 +7,16 @@ namespace PadawansTask2
     {
         public static int Calculate(int number, int add, int count)
         {
-            // put your code here
-            throw new NotImplementedException();
+            int sum = 0;
+            int[] mas = new int[count];
+            int rez = 0;
+            for (int i = 0; i < count; i++)
+            {
+                sum = number + (i * add);
+                mas[i] = sum;
+                rez = mas.Sum();
+            }
+            return rez;
         }
     }
 }
